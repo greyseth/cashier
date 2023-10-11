68911f868805fb3d84f374a948b7664b21a087a7
greyseth <anargya2gilland@gmail.com>
2023-10-11 11:30:53 +0700

    Added PostgreSQL dependency

--- src/main/java/com/smk/cashier/App.java
+++ src/main/java/com/smk/cashier/App.java
@@ -7,23 +7,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        User user = new User();
-        user.setId(0);
-        user.setUsername("Grey");
-        user.setPassword("1234");
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("---Cashier app 3000---\nLog in to access the application:");
-        System.out.println("Username: ");
-        String username = scanner.next();
-        System.out.println("Password: ");
-        String password = scanner.next();
-
-        if (username.equals(user.getUsername()) && password.equals(user.getPassword())) {
-            System.out.println("Welcome, "+user.getUsername()+".");
-        }else {
-            System.out.println("Invalid credentials");
-        }
+        System.out.println("---Cashier app 3000---");
     }
 }
